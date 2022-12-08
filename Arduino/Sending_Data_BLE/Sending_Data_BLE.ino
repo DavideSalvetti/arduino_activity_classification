db6d34fd2165b8a032cc8800b636474c29be5384
Matteo Verzeroli <matteoverzeroli@live.it>
2022-12-08 22:11:40 +0100

    revision of firmware: now if data non available send previous data. Todo: add timer implementation

--- Arduino/Sending_Data_BLE/Sending_Data_BLE.ino
+++ Arduino/Sending_Data_BLE/Sending_Data_BLE.ino
@@ -3,6 +3,10 @@
 #include "Nano33BLEAccelerometer.h"
 #include "Nano33BLEGyroscope.h"
 #include "Nano33BLETemperature.h"
+#include "string.h"
+//#include "Timer.h"
+
+//Timer t;
 
 Nano33BLEAccelerometerData accelerometerData;
 Nano33BLEGyroscopeData gyroscopeData;
@@ -12,11 +16,16 @@ BLEService accGyroTempHumiService("e2e65ffc-5687-4cbe-8f2d-db76265f269f");
 BLEStringCharacteristic sensorCharacteristic("3000", BLERead | BLENotify, 150);
 
 unsigned long previousMillis = 0;
-unsigned long incrementalVal = 0;
+
+char token[150] = "";
+char accstr[50] = "";
+char gyrostr[50] = "";
+char tempstr[10] = "";
+char timestr[40] = "";
+
 
 void setup() {
-  Serial.begin(9600);
-  while(!Serial);
+  Serial.begin(115200);
 
   Accelerometer.begin();
   Gyroscope.begin();
@@ -26,7 +35,6 @@ void setup() {
 
   if(!BLE.begin()){
     Serial.println("starting BLE failed!");
-
     while(true);
   }
 
@@ -49,21 +57,27 @@ void loop() {
 
     digitalWrite(LED_BUILTIN, HIGH);
 
+    int num_timer;
+
     while(central.connect()){
       unsigned long currentMillis = millis();
 
-      if(currentMillis - previousMillis >= 10){
+      //num_timer=t.every(1000,updateSensors);
+     if(currentMillis - previousMillis >= 10){
         previousMillis = currentMillis;
-        incrementalVal++;
 
         updateSensors();
       }
     }
 
+    //t.stop(num_timer);
+
     digitalWrite(LED_BUILTIN, LOW);
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
   }
+
+  //t.update();
 }
 
 /*  Funzione per l'aggiornamento dei valori della caratteristica. 
@@ -71,29 +85,28 @@ void loop() {
  *  Per verificare il timing, inserisce un valore incrementale alla fine.
  */
 void updateSensors() {
+  sprintf(token,"");
 
-  char data[150];
-  memset(data, 0, sizeof(data));
   if (Accelerometer.pop(accelerometerData)) {
-    sprintf(data, "%.3f,%.3f,%.3f,", accelerometerData.x, accelerometerData.y, accelerometerData.z);
+    sprintf(accstr, "%.3f,%.3f,%.3f,", accelerometerData.x, accelerometerData.y, accelerometerData.z);
   }
-
-  int len = strlen(data);
 
   if (Gyroscope.pop(gyroscopeData)) {
-    sprintf(&data[len], "%.3f,%.3f,%.3f,", gyroscopeData.x, gyroscopeData.y, gyroscopeData.z);
+    sprintf(gyrostr, "%.3f,%.3f,%.3f,", gyroscopeData.x, gyroscopeData.y, gyroscopeData.z);
   }
-
-  len = strlen(data);
 
   if (Temperature.pop(temperatureData)) {
-    sprintf(&data[len], "%.3f,%.3f,", temperatureData.temperatureCelsius, temperatureData.humidity);
+    sprintf(tempstr, "%.f,%.f,", temperatureData.temperatureCelsius, temperatureData.humidity);
   }
 
-  len = strlen(data);
+  sprintf(timestr, "%i", previousMillis);
 
-  sprintf(&data[len], "%i", incrementalVal);
+  strcat(token, accstr);
+  strcat(token, gyrostr);
+  strcat(token, tempstr);
+  strcat(token, timestr);
 
-  sensorCharacteristic.writeValue(data);
+  //Serial.println(token);
 
+  sensorCharacteristic.writeValue(token);
 }

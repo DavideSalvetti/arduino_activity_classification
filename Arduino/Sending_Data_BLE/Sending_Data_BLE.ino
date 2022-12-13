f9f68366c96de874d56214b75b90fccf1a3e5114
Matteo Verzeroli <matteoverzeroli@live.it>
2022-12-13 10:41:16 +0100

    added sampling with interrupt

--- Arduino/Sending_Data_BLE/Sending_Data_BLE.ino
+++ Arduino/Sending_Data_BLE/Sending_Data_BLE.ino
@@ -12,7 +12,7 @@ Nano33BLETemperatureData temperatureData;
 BLEService accGyroTempHumiService("e2e65ffc-5687-4cbe-8f2d-db76265f269f");
 BLEStringCharacteristic sensorCharacteristic("3000", BLERead | BLENotify, 150);
 
-unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
 
 char token[150] = "";
 char accstr[50] = "";
@@ -27,9 +27,12 @@ BLEDevice central;
 #include "NRF52_MBED_TimerInterrupt.h"
 #include "NRF52_MBED_ISR_Timer.h"
 #define HW_TIMER_INTERVAL_MS      1
-NRF52_MBED_Timer ITimer(NRF_TIMER_3);
+NRF52_MBED_Timer ITimer(NRF_TIMER_1);
 NRF52_MBED_ISRTimer ISR_Timer;
-#define TIMER_INTERVAL_1S             1000L
+#define TIMER_INTERVAL_1S             10L
+
+bool mutex= false;
+
 void TimerHandler()
 {
   ISR_Timer.run();
@@ -53,73 +56,47 @@ void setup() {
 
   BLE.advertise();
 
+  Serial.begin(115200);
 
   ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_MS * 1000, TimerHandler);
   ISR_Timer.setInterval(TIMER_INTERVAL_1S,  updateSensors);
-  BLEDevice central = BLE.central();
 }
 
 void loop() {
   central = BLE.central();
-/*
-  if(central){
-    Serial.print("Connected to central: ");
-    Serial.println(central.address());
 
-    digitalWrite(LED_BUILTIN, HIGH);
+  if(mutex){
 
+    sprintf(token,"");
 
+    sprintf(accstr, "%.3f,%.3f,%.3f,", accelerometerData.x, accelerometerData.y, accelerometerData.z);
+    sprintf(gyrostr, "%.3f,%.3f,%.3f,", gyroscopeData.x, gyroscopeData.y, gyroscopeData.z);
+    sprintf(tempstr, "%.f,%.f,", temperatureData.temperatureCelsius, temperatureData.humidity);
 
-    while(central.connect()){
-      unsigned long currentMillis = millis();
+    sprintf(timestr, "%i", currentMillis);
 
-      //num_timer=t.every(1000,updateSensors);
-     if(currentMillis - previousMillis >= 10){
-        previousMillis = currentMillis;
+    strcat(token, accstr);
+    strcat(token, gyrostr);
+    strcat(token, tempstr);
+    strcat(token, timestr);
 
-        updateSensors();
-      }
+    Serial.println(token);
+
+    if(central){
+     sensorCharacteristic.writeValue(token);
     }
-
-    //t.stop(num_timer);
-  
-
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.print("Disconnected from central: ");
-    Serial.println(central.address());
+    mutex = false;
   }
-*/
-  //t.update();
 }
 
-/*  Funzione per l'aggiornamento dei valori della caratteristica. 
- *  Aggiorna la caratteristica con i valori di accelerometro, giroscopio, temperatura e umidità.
- *  Per verificare il timing, inserisce un valore incrementale alla fine.
- */
 void updateSensors() {
-  sprintf(token,"");
+  if(!mutex){
+    mutex = true;
 
-  if (Accelerometer.pop(accelerometerData)) {
-    sprintf(accstr, "%.3f,%.3f,%.3f,", accelerometerData.x, accelerometerData.y, accelerometerData.z);
+    Accelerometer.pop(accelerometerData);
+    Gyroscope.pop(gyroscopeData);
+    Temperature.pop(temperatureData);
+
+    currentMillis = millis();
   }
-
-  if (Gyroscope.pop(gyroscopeData)) {
-    sprintf(gyrostr, "%.3f,%.3f,%.3f,", gyroscopeData.x, gyroscopeData.y, gyroscopeData.z);
-  }
-
-  if (Temperature.pop(temperatureData)) {
-    sprintf(tempstr, "%.f,%.f,", temperatureData.temperatureCelsius, temperatureData.humidity);
-  }
-
-  sprintf(timestr, "%i", previousMillis);
-
-  strcat(token, accstr);
-  strcat(token, gyrostr);
-  strcat(token, tempstr);
-  strcat(token, timestr);
-
-  //Serial.println(token);
-  /*
-  if(central && central.connect())
-    sensorCharacteristic.writeValue(token);*/
 }

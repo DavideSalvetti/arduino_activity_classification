2359d01e16c66f2398c21ab3b677dede08c48bf8
Matteo Verzeroli <matteoverzeroli@live.it>
2022-12-30 15:19:35 +0100

    refactor of firmware for classification: now acquire data is a thread; added circular buffer for optimization

--- arduino/activity_classification/activity_classification.ino
+++ arduino/activity_classification/activity_classification.ino
@@ -1,227 +1,130 @@
-/* Edge Impulse ingestion SDK
- * Copyright (c) 2022 EdgeImpulse Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+#define CIRCULAR_BUFFER_INT_SAFE
 
-/* Includes ---------------------------------------------------------------- */
 #include <arduino_activity_classification_inferencing.h>
-#include <Arduino_LSM9DS1.h> //Click here to get the library: http://librarymanager/All#Arduino_LSM9DS1
+#include <Arduino_LSM9DS1.h>
+//#include <C:\Users\Davide\Documents\Arduino\libraries\CircularBuffer\CircularBuffer.h>
+#include <C:\Users\Matteo\Documents\Arduino\libraries\CircularBuffer\CircularBuffer.h>
 
-#include "Nano33BLEAccelerometer.h"
-#include "Nano33BLEGyroscope.h"
 
-/* Constant defines -------------------------------------------------------- */
-#define CONVERT_G_TO_MS2    9.80665f
-#define MAX_ACCEPTED_RANGE  2.0f        // starting 03/2022, models are generated setting range to +-2, but this example use Arudino library which set range to +-4g. If you are using an older model, ignore this value and use 4.0f instead
+static bool debug_nn = true;
+static uint16_t run_inference_every_ms = 5000;
+uint16_t buffer_step = 0;
 
-/*
- ** NOTE: If you run into TFLite arena allocation issue.
- **
- ** This may be due to may dynamic memory fragmentation.
- ** Try defining "-DEI_CLASSIFIER_ALLOCATION_STATIC" in boards.local.txt (create
- ** if it doesn't exist) and copy this file to
- ** `<ARDUINO_CORE_INSTALL_PATH>/arduino/hardware/<mbed_core>/<core_version>/`.
- **
- ** See
- ** (https://support.arduino.cc/hc/en-us/articles/360012076960-Where-are-the-installed-cores-located-)
- ** to find where Arduino installs cores on your machine.
- **
- ** If the problem persists then there's not enough memory for this model and application.
- */
-
-/* Private variables ------------------------------------------------------- */
-static bool debug_nn = true; // Set this to true to see e.g. features generated from the raw signal
-static uint32_t run_inference_every_ms = 5000;
-static rtos::Thread inference_thread(osPriorityLow);
-static float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
 static float inference_buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE];
+CircularBuffer<float, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE> bufferCircolare;
 
-/* Last data read from sensors, used in case the value is not ready*/
-Nano33BLEAccelerometerData lastAccData;
-Nano33BLEGyroscopeData lastGyroData;
+static rtos::Thread inference_thread(osPriorityRealtime);
+static rtos::Thread dataread_thread(osPriorityRealtime);
 
-/* Forward declaration */
 void run_inference_background();
+void get_IMU_data();
 
-/**
-* @brief      Arduino setup function
-*/
-void setup()
-{
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    // comment out the below line to cancel the wait for USB connection (needed for native USB)
-    while (!Serial);
-    Serial.println("Edge Impulse Inferencing Demo");
+void setup() {
+  Serial.begin(115200);
 
-    // Accelerometer.begin();
-    // Gyroscope.begin();
+  delay(5000);
 
-    if (!IMU.begin()) {
-        ei_printf("Failed to initialize IMU!\r\n");
-    }
-    else {
-        ei_printf("IMU initialized\r\n");
-    }
+  if (!IMU.begin()) {
+    ei_printf("Failed to initialize IMU!\r\n");
+  } else {
+    ei_printf("IMU initialized\r\n");
+  }
+  if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != 5) {
+    ei_printf("ERR: EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME should be equal to 5 (3 acc + 2 gyro)\n");
+    return;
+  }
 
-    // if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != 3) {
-    //     ei_printf("ERR: EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME should be equal to 3 (the 3 sensor axes)\n");
-    //     return;
-    // }
-
-    inference_thread.start(mbed::callback(&run_inference_background));
+  inference_thread.start(mbed::callback(&run_inference_background));
+  dataread_thread.start(mbed::callback(&get_IMU_data));
 }
 
-/**
- * @brief Return the sign of the number
- * 
- * @param number 
- * @return int 1 if positive (or 0) -1 if negative
- */
-float ei_get_sign(float number) {
-    return (number >= 0.0) ? 1.0 : -1.0;
-}
+void run_inference_background() {
+  // wait until we have a full buffer
+  rtos::Thread::signal_wait(0x1);
 
-/**
- * @brief      Run inferencing in the background.
- */
-void run_inference_background()
-{
-    // wait until we have a full buffer
-    delay((EI_CLASSIFIER_INTERVAL_MS * EI_CLASSIFIER_RAW_SAMPLE_COUNT) + 100);
+  // This is a structure that smoothens the output result
+  // With the default settings 70% of readings should be the same before classifying.
+  ei_classifier_smooth_t smooth;
+  ei_classifier_smooth_init(&smooth, 10 /* no. of readings */, 7 /* min. readings the same */, 0.8 /* min. confidence */, 0.3 /* max anomaly */);
 
-    // This is a structure that smoothens the output result
-    // With the default settings 70% of readings should be the same before classifying.
-    ei_classifier_smooth_t smooth;
-    ei_classifier_smooth_init(&smooth, 10 /* no. of readings */, 7 /* min. readings the same */, 0.8 /* min. confidence */, 0.3 /* max anomaly */);
+  while (1) {
 
-    while (1) {
-        // copy the buffer
-        memcpy(inference_buffer, buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE * sizeof(float));
-
-        // Turn the raw buffer in a signal which we can the classify
-        signal_t signal;
-        int err = numpy::signal_from_buffer(inference_buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
-        if (err != 0) {
-            ei_printf("Failed to create signal from buffer (%d)\n", err);
-            return;
-        }
-
-        // Run the classifier
-        ei_impulse_result_t result = { 0 };
-
-        err = run_classifier(&signal, &result, debug_nn);
-        if (err != EI_IMPULSE_OK) {
-            ei_printf("ERR: Failed to run classifier (%d)\n", err);
-            return;
-        }
-
-        // print the predictions
-        ei_printf("Predictions ");
-        ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
-            result.timing.dsp, result.timing.classification, result.timing.anomaly);
-        ei_printf(": ");
-
-        // ei_classifier_smooth_update yields the predicted label
-        const char *prediction = ei_classifier_smooth_update(&smooth, &result);
-        ei_printf("%s ", prediction);
-        // print the cumulative results
-        ei_printf(" [ ");
-        for (size_t ix = 0; ix < smooth.count_size; ix++) {
-            ei_printf("%u", smooth.count[ix]);
-            if (ix != smooth.count_size + 1) {
-                ei_printf(", ");
-            }
-            else {
-              ei_printf(" ");
-            }
-        }
-        ei_printf("]\n");
-
-        delay(run_inference_every_ms);
+    for(unsigned short i = 0; i < bufferCircolare.size(); i++){
+      inference_buffer[i] = bufferCircolare[i];
     }
 
-    ei_classifier_smooth_free(&smooth);
+    // Turn the raw buffer in a signal which we can the classify
+    signal_t signal;
+    int err = numpy::signal_from_buffer(inference_buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+    if (err != 0) {
+      ei_printf("Failed to create signal from buffer (%d)\n", err);
+      return;
+    }
+
+    // Run the classifier
+    ei_impulse_result_t result = { 0 };
+
+    err = run_classifier(&signal, &result, debug_nn);
+    if (err != EI_IMPULSE_OK) {
+      ei_printf("ERR: Failed to run classifier (%d)\n", err);
+      return;
+    }
+
+    // print the predictions
+    ei_printf("Predictions ");
+    ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
+              result.timing.dsp, result.timing.classification, result.timing.anomaly);
+    ei_printf(": ");
+
+    // ei_classifier_smooth_update yields the predicted label
+    const char *prediction = ei_classifier_smooth_update(&smooth, &result);
+    ei_printf("%s ", prediction);
+    // print the cumulative results
+    ei_printf(" [ ");
+    for (size_t ix = 0; ix < smooth.count_size; ix++) {
+      ei_printf("%u", smooth.count[ix]);
+      if (ix != smooth.count_size + 1) {
+        ei_printf(", ");
+      } else {
+        ei_printf(" ");
+      }
+    }
+    ei_printf("]\n");
+
+    rtos::Thread::wait(run_inference_every_ms);
+  }
+
+  ei_classifier_smooth_free(&smooth);
 }
 
-/**
-* @brief      Get data and run inferencing
-*
-* @param[in]  debug  Get debug info if true
-*/
-void loop()
-{
-    while (1) {
-        // Determine the next tick (and then sleep later)
-        uint64_t next_tick = micros() + (EI_CLASSIFIER_INTERVAL_MS * 1000);
+void get_IMU_data() {
+  float accx, accy, accz, gyrox, gyroy, gyroz;
+  while (1) {
 
-        // roll the buffer -5 points so we can overwrite the last one
-        numpy::roll(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, -5);
-
-        // Nano33BLEAccelerometerData accData;
-        // Nano33BLEGyroscopeData gyroData;
-
-        // if(Accelerometer.pop(accData)) {
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 5] = accData.x;
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 4] = accData.y;
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3] = accData.z;
-
-        //   lastAccData = accData;
-        // } else {
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 5] = lastAccData.x;
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 4] = lastAccData.y;
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3] = lastAccData.z;
-        // }
-          
-
-        // if(Gyroscope.pop(gyroData)) {
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 2] = gyroData.y;
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 1] = gyroData.z;
-
-        //   lastGyroData = gyroData;
-        // } else {
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 5] = lastGyroData.y;
-        //   buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 4] = lastGyroData.z;
-        // }
-
-        //read to the end of the buffer
-        IMU.readAcceleration(
-            buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 5],
-            buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 4],
-            buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3]
-        );
-
-        // for (int i = 0; i < 3; i++) {
-        //     if (fabs(buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3 + i]) > MAX_ACCEPTED_RANGE) {
-        //         buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3 + i] = ei_get_sign(buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 3 + i]) * MAX_ACCEPTED_RANGE;
-        //     }
-        // }
-  	    float tmp;
-        IMU.readGyroscope( 
-            tmp,
-            buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 2],
-            buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 1]
-        );
-
-        //ei_printf("x acc (%f)\n", buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE - 5]);
-
-        // and wait for next tick
-        uint64_t time_to_wait = next_tick - micros();
-        delay(15);
-        //ei_printf("next_tick (%d) time_to_wait (%d) eff (%d) \n", next_tick, time_to_wait, time_to_wait%1000);
-        //delayMicroseconds(time_to_wait % 1000);
+    if (IMU.accelerationAvailable()) {
+      IMU.readAcceleration(accx,accy,accz);
+    } 
+    if (IMU.gyroscopeAvailable()) {
+       IMU.readGyroscope(gyrox,gyroy,gyroz);
     }
+
+    bufferCircolare.push(accx);
+    bufferCircolare.push(accy);
+    bufferCircolare.push(accz);
+    bufferCircolare.push(gyroy);
+    bufferCircolare.push(gyroz);
+
+    if (buffer_step < EI_CLASSIFIER_RAW_SAMPLE_COUNT - 1) {
+      buffer_step++;
+    } else if (buffer_step != 2000) {
+      buffer_step++;
+      inference_thread.signal_set(0x1);
+    }
+    rtos::Thread::wait(15);
+  }
+}
+
+void loop() {
 }
 
 #if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_FUSION
---
+++ misure/untitled.m
@@ -0,0 +1,20 @@
+s = serial('COM7','BAU',115200,'terminator','LF');
+fopen(s);
+
+arr2 = nan(100,1);
+
+for i=1:100
+    arr2(i) = fscanf(s,'%d');
+    pause(0.015)
+end
+
+fclose(s);
+
+
+
+%%
+figure
+d_10 = diff(arr2(:,1));
+plot(d_10,'o');
+m_10=mean(d_10)
+s_10 = std(d_10)

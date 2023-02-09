174349ce304e969f3fa220f7a82a4b6dd3c684b4
Matteo Verzeroli <matteoverzeroli@live.it>
2023-02-09 15:07:21 +0100

    review of data acquisition paragraph

--- misure/plot_misure.m
+++ misure/plot_misure.m
@@ -2,27 +2,62 @@
 %exampledata_filtr = movmean(exampledata_filtr,10,2);
 
 
-exampledata_filtr = lowpass(exampledata,10,1000/15);
-exampledata_filtr = highpass(exampledata_filtr,0.5,1000/15);
-exampledata_filtr(:,9) = exampledata(:,9) - exampledata(1,9);
+exampledata_filtr = lowpass(exampledata,20,1000/15);
+%exampledata_filtr = highpass(exampledata_filtr,0.5,1000/15);
+exampledata_filtr(:,9) = ((exampledata(:,9) - exampledata(1,9))./1000)-5;
 
-figure 
-hold on
-plot(exampledata_filtr(:,9),exampledata_filtr(:,1));
-plot(exampledata_filtr(:,9),exampledata_filtr(:,2));
-plot(exampledata_filtr(:,9),exampledata_filtr(:,3));
-hold off
-legend('accX', 'accY', 'accZ')
-xlim([5000 15000])
+figure
+sgtitle('IMU data 20Hz LPF')
+subplot(2,3,1)
+plot(exampledata_filtr(:,9),exampledata_filtr(:,1),'Color','red');
+xlim([0 10])
+xlabel('Time [s]')
+ylabel('Magnitudo [g]')
+legend('accX','Location','south')
+subplot(2,3,2)
+plot(exampledata_filtr(:,9),exampledata_filtr(:,2),'Color','green');
+xlim([0 10])
+xlabel('Time [s]')
+ylabel('Magnitudo [g]')
+legend('accY','Location','south')
+subplot(2,3,3)
+plot(exampledata_filtr(:,9),exampledata_filtr(:,3),'Color','blu');
+xlim([0 10])
+xlabel('Time [s]')
+ylabel('Magnitudo [g]')
+legend('accZ','Location','south')
 
-figure 
-hold on
+subplot(2,3,4)
 plot(exampledata_filtr(:,9),exampledata_filtr(:,4));
+xlim([0 10])
+xlabel('Time [s]')
+ylabel('Magnitudo [dps]')
+legend('gyroX','Location','south')
+subplot(2,3,5)
 plot(exampledata_filtr(:,9),exampledata_filtr(:,5));
+xlim([0 10])
+xlabel('Time [s]')
+ylabel('Magnitudo [dps]')
+legend('gyroY','Location','south')
+subplot(2,3,6)
 plot(exampledata_filtr(:,9),exampledata_filtr(:,6));
-hold off
-legend('gyroX', 'gyroY', 'gyroZ')
-xlim([5000 15000])
+xlim([0 10])
+xlabel('Time [s]')
+ylabel('Magnitudo [dps]')
+legend('gyroZ','Location','south')
+
+figure
+d_1 = diff(exampledata(:,9));
+plot(d_1,'o');
+ylim([13 31])
+xlim([0 5067])
+xlabel('sample')
+ylabel('Time [ms]')
+title('Acquisition interval between two samples')
+
+m_1=mean(d_1)
+s_1 = std(d_1)
+
 
 figure 
 hold on
@@ -39,9 +74,3 @@ plot(exampledata(:,9),exampledata(:,4));
 plot(exampledata(:,9),exampledata(:,5));
 plot(exampledata(:,9),exampledata(:,6));
 hold off
-
-figure
-d_1 = diff(exampledata(:,9));
-plot(d_1,'o');
-m_1=mean(d_1)
-s_1 = std(d_1)
